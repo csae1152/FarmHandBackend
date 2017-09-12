2730297168af963d1fb6bd281fa51f25f6a87d73
Helmut Steiner <csae1152@uibk.ac.at>
2017-09-12 15:12:34 +0200

    Update FarmHandVerticle.java

--- Desktop/Helmut/FarmHand/FarmHand/src/main/java/com/farmhand/clustering/FarmHandVerticle.java
+++ Desktop/Helmut/FarmHand/FarmHand/src/main/java/com/farmhand/clustering/FarmHandVerticle.java
@@ -11,13 +11,13 @@ import java.util.UUID;
  */
 public class FarmHandVerticle extends AbstractVerticle {
     private static final String ID = UUID.randomUUID().toString();
-    private static final String message = "Hello from here";
+    private static final String message = "Here";
     private static final String body = "<h1>From port 80</h1>";
     
     
    @Override
   public void start(Future<Void> startFuture) throws Exception {
-    vertx.eventBus().<String>consumer("hello", message -> {
+    vertx.eventBus().<String>consumer("hello", name -> {
       message.reply("Hello " + message.body() + " from " + ID);
     }).completionHandler(startFuture.completer());
   }
